867cc82cebc840fcc0332050bed8ec9afac7e20f
Scott Chamberlain <myrmecocystus@gmail.com>
2012-04-08 23:51:33 -0500

    four new fxns

---
+++ R/getsynonymnamesfromtsn.R
@@ -0,0 +1,37 @@
+#' Returns a list of the synonyms (if any) for the TSN.
+#' @import XML RCurl plyr
+#' @param tsn TSN for a taxonomic group (numeric)
+#' @param url the ITIS API url for the function (should be left to default)
+#' @param ... optional additional curl options (debugging tools mostly)
+#' @param curl If using in a loop, call getCurlHandle() first and pass 
+#'  the returned value in here (avoids unnecessary footprint)
+#' @return A data.frame with results.
+#' @export
+#' @examples \dontrun{
+#' getsynonymnamesfromtsn(tsn = 183671) # tsn not accepted
+#' getsynonymnamesfromtsn(tsn = 526852) # tsn accepted
+#' }
+getsynonymnamesfromtsn <- function(tsn = NA,
+  url = 'http://www.itis.gov/ITISWebService/services/ITISService/getSynonymNamesFromTSN',
+  ..., curl = getCurlHandle() ) 
+{
+  args <- list()
+  if(!is.na(tsn))
+    args$tsn <- tsn
+  message(paste(url, '?tsn=', tsn, sep=''))
+  tt <- getForm(url,
+    .params = args,
+    ...,
+    curl = curl)
+  out <- xmlParse(tt)
+  namespaces <- c(ax23="http://data.itis_service.itis.usgs.org/xsd")
+  nodes <- getNodeSet(out, "//ax23:name", namespaces=namespaces)
+  if( length(sapply(nodes, xmlValue)) == 0){ name <- list("nomatch") } else
+    { name <- sapply(nodes, xmlValue) }
+  nodes <- getNodeSet(out, "//ax23:tsn", namespaces=namespaces)
+  if( length(sapply(nodes, xmlValue)) == 1){ tsn <- sapply(nodes, xmlValue) } else
+    { tsn <- sapply(nodes, xmlValue) 
+      tsn <- tsn[-length(tsn)]
+    } 
+  data.frame(name=name[[1]], tsn=tsn)
+}
---
+++ R/gettaxonauthorshipfromtsn.R
@@ -0,0 +1,34 @@
+#' Returns the author information for the TSN.
+#' @import XML RCurl plyr
+#' @param tsn TSN for a taxonomic group (numeric)
+#' @param url the ITIS API url for the function (should be left to default)
+#' @param ... optional additional curl options (debugging tools mostly)
+#' @param curl If using in a loop, call getCurlHandle() first and pass 
+#'  the returned value in here (avoids unnecessary footprint)
+#' @return A data.frame with results.
+#' @export
+#' @examples \dontrun{
+#' gettaxonauthorshipfromtsn(tsn = 183671)
+#' }
+gettaxonauthorshipfromtsn <- function(tsn = NA,
+  url = 'http://www.itis.gov/ITISWebService/services/ITISService/getTaxonAuthorshipFromTSN',
+  ..., curl = getCurlHandle() ) 
+{
+  args <- list()
+  if(!is.na(tsn))
+    args$tsn <- tsn
+  message(paste(url, '?tsn=', tsn, sep=''))
+  tt <- getForm(url,
+    .params = args,
+#     ...,
+    curl = curl)
+  out <- xmlParse(tt)
+  namespaces <- c(ax23="http://data.itis_service.itis.usgs.org/xsd")
+  toget <- list("authorship","updateDate","tsn")
+  xpathfunc <- function(x) {    
+    sapply(getNodeSet(out, paste("//ax23:", x, sep=''), namespaces=namespaces),xmlValue)
+  }
+  df <-  do.call(cbind, laply(toget, as.data.frame(xpathfunc)))
+  names(df) <- toget
+  df
+}
