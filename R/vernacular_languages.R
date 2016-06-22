0cd64809c36f769406f6c3a0c2e13962aeaeb4dd
Scott Chamberlain <myrmecocystus@gmail.com>
2016-06-22 13:14:13 -0700

    fix to vernacular_languages for tibble as_Data_Frame no method for character

--- R/vernacular_languages.R
+++ R/vernacular_languages.R
@@ -2,12 +2,12 @@
 #'
 #' @export
 #' @template common
-#' @return a data.frame
+#' @return a character vector of verncular names
 #' @examples \dontrun{
 #' vernacular_languages()
 #' }
 vernacular_languages <- function(wt = "json", raw = FALSE, ...) {
   out <- itis_GET("getVernacularLanguages", list(), wt, ...)
   if (raw || wt == "xml") return(out)
-  tibble::as_data_frame(parse_raw(out)$languageNames)
+  parse_raw(out)$languageNames
 }
