7e07913cfc1fa62485d1c9f53d6565fd20bf4a3b
Scott Chamberlain <myrmecocystus@gmail.com>
2019-10-23 09:55:53 -0700

    fix examples in itis_search for space problems

--- R/itis_search.R
+++ R/itis_search.R
@@ -13,22 +13,28 @@
 #' @examples \dontrun{
 #' itis_search(q = "tsn:182662")
 #'
-#' itis_search(q = "nameWOInd:Liquidamber\\%20styraciflua~0.4")
+#' itis_search(q = "nameWOInd:Liquidamber\\ styraciflua~0.4")
+#' 
 #' # matches only monomials
-#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[%20]{0,0}*/")
+#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[ ]{0,0}*/")
+#' 
 #' # matches only binomials
-#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[%20]{1,1}[A-Za-z0-9]*/")
+#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[ ]{1,1}[A-Za-z0-9]*/")
+#' 
 #' # matches only trinomials
-#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[%20]{1,1}[A-Za-z0-9]*[%20]{1,1}[A-Za-z0-9]*/")
+#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[ ]{1,1}[A-Za-z0-9]*[ ]{1,1}[A-Za-z0-9]*/")
+#' 
 #' # matches binomials or trinomials
-#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[%20]{1,1}[A-Za-z0-9]*[%20]{0,1}[A-Za-z0-9]*/")
+#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[ ]{1,1}[A-Za-z0-9]*[ ]{0,1}[A-Za-z0-9]*/")
 #'
 #' itis_search(q = "nameWOInd:Poa\\ annua")
 #'
-#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[%20]{0,0}*/", rows = 2)
-#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[%20]{0,0}*/", rows = 200)
+#' # pagination
+#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[ ]{0,0}*/", rows = 2)
+#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[ ]{0,0}*/", rows = 200)
 #'
-#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[%20]{0,0}*/",
+#' # select fields to return
+#' itis_search(q = "nameWOInd:/[A-Za-z0-9]*[ ]{0,0}*/",
 #'    fl = c('nameWInd', 'tsn'))
 #' }
 itis_search <- function(..., proxy = NULL, callopts=list()) {
